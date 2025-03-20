eb5d63af8f39ff7b2d33ec0cd25bfb2fbca13ad6
isaac-adebayo <ai.isaacadebayo@gmail.com>
2025-03-20 06:44:11 +0100

    modified app.js file

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-console.log("Hello, Demo!");
+const express = require('express');
+const app = express();
 
-var moment = require('moment');
-var date = moment().format('LL');
-console.log(date);
+const PORT = 8080;
+
+// Define a route for the root URL "/"
+app.get('/', (req, res) => {
+    res.send('<h3>Hello World</h3><p>This test page tests Github Action CI');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
